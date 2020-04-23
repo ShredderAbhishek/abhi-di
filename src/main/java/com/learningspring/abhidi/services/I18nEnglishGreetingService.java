f7b1617eddeb3cd5c500f0094d40114e98e46099
abhishek kumar <abhishek.kumar@247.ai>
2020-04-24 01:01:42 +0530

    modifying EN and ES profiles' configuration

--- src/main/java/com/learningspring/abhidi/services/I18nEnglishGreetingService.java
+++ src/main/java/com/learningspring/abhidi/services/I18nEnglishGreetingService.java
@@ -2,7 +2,7 @@ package com.learningspring.abhidi.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18nGreetingService")
 public class I18nEnglishGreetingService implements GreetingService {
     @Override
--- src/main/java/com/learningspring/abhidi/services/I18nSpanishGreetingService.java
+++ src/main/java/com/learningspring/abhidi/services/I18nSpanishGreetingService.java
@@ -2,7 +2,7 @@ package com.learningspring.abhidi.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("ES")
+@Profile({"ES"})
 @Service("i18nGreetingService")
 public class I18nSpanishGreetingService implements GreetingService {
     @Override
